8cfbc0b273b3b22387fd39fd30be97fc6f0a4f99
FireGhostPhino <Jackspaar@outlook.at>
2024-09-09 17:33:17 +0200

    Erste eigene Get-Funktion

---
+++ SWKOM DocManager/SWKOM/Controllers/DocumentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace SWKOM.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentController : ControllerBase
+    {
+        private readonly ILogger<DocumentController> _logger;
+
+        public DocumentController(ILogger<DocumentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetDocument")]
+        public string Get()
+        {
+            var documentInformation = new DocumentInformation
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Title = "Title 1",
+                Author = "Me",
+                Content = "Line 1."
+            };
+
+            return JsonSerializer.Serialize(documentInformation);
+        }
+    }
+}
---
+++ SWKOM DocManager/SWKOM/DocumentInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace SWKOM
+{
+    public class DocumentInformation
+    {
+        public DateOnly Date { get; set; }
+
+        public string Title { get; set; } = "";
+
+        public string? Author { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
