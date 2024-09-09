6d8fb2b7d9b56fd669e1bfbdbe9f83f239d39c2a
FireGhostPhino <Jackspaar@outlook.at>
2024-09-09 17:37:54 +0200

    eigene Post, Get, Put drin

--- SWKOM DocManager/SWKOM/Controllers/DocumentController.cs
+++ SWKOM DocManager/SWKOM/Controllers/DocumentController.cs
@@ -14,8 +14,36 @@ namespace SWKOM.Controllers
             _logger = logger;
         }
 
+        [HttpPost(Name = "GetDocument")]
+        public string Post()
+        {
+            var documentInformation = new DocumentInformation
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Title = "Title 1",
+                Author = "Me",
+                Content = "Line 1."
+            };
+
+            return JsonSerializer.Serialize(documentInformation);
+        }
+
         [HttpGet(Name = "GetDocument")]
         public string Get()
+        {
+            var documentInformation = new DocumentInformation
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Title = "Title 1",
+                Author = "Me",
+                Content = "Line 1."
+            };
+
+            return JsonSerializer.Serialize(documentInformation);
+        }
+
+        [HttpPut(Name = "GetDocument")]
+        public string Put()
         {
             var documentInformation = new DocumentInformation
             {
