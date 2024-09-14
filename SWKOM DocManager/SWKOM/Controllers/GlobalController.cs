4e4fdb06393d3522043d39afa13892b1985c2d3c
FireGhostPhino <Jackspaar@outlook.at>
2024-09-14 12:41:51 +0200

    Global Response added

---
+++ SWKOM DocManager/SWKOM/Controllers/GlobalController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWKOM.Models;
+
+namespace SWKOM.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class GlobalController : ControllerBase
+    {
+        private readonly ILogger<GlobalController> _logger;
+
+        public GlobalController(ILogger<GlobalController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetGlobalAllDocuments")]
+        public ActionResult<IEnumerable<DocumentInformation>> Get()
+        {
+
+            var documents = Enumerable.Range(1, 5).Select(index => new DocumentInformation
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Title = "Title 1",
+                Author = "Me",
+                Content = "Line 1.",
+                Id = Guid.NewGuid()
+            })
+            .ToArray();
+
+            return Ok(documents);
+        }
+    }
+}
