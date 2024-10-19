417d4b98b0741cbd990259282d6b026101431c48
FireGhostPhino <Jackspaar@outlook.at>
2024-10-19 11:59:19 +0200

    Empty UnitTest Project created

---
+++ SWKOM DocManager/UnitTests/WebUITests/UnitTestWebUI.cs
@@ -0,0 +1,16 @@
+namespace UnitTests.WebUITests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
